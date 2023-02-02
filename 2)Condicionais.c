a4fb8accf41fef21b34ac4715109c1f14150545f
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2023-02-02 19:22:50 -0300

    Create 2)Condicionais.c

---
+++ 2)Condicionais.c
@@ -0,0 +1,102 @@
+
+#1)	Solicitar duas notas para o cálculo da média, mostrar a média e se a média for Maior ou igual a 6, mostrar “Aprovado”, caso contrário mostrar “Reprovado”.
+
+Int main()
+{
+   float  nota1=0, nota2=0, media=0;
+   cout<<"\nPrograma de calculo de media\n";
+   cout<<"Digite a primeira nota:";
+   cin>> nota1;
+   cout<<"Digite a segunda nota:";
+   cin>> nota2;
+  media=(nota1+nota2)/2;
+
+   cout<<"\nMedia: "<<media;
+
+  if(media>=6)
+
+  {
+  cout<<"\nAPROVADO\n";
+  }
+  else
+  {
+  cout<<"\nREPROVADO\n";
+  }
+}
+===========================================================================================================
+#2) Solicitar 3 valores inteiros para o cálculo da equação de segundo grau, 
+     
+ #DELTA = B AO QUADRADO -4.a.c 
+
+#Se DELTA < 0 -> não existem raízes reais
+
+#Se DELTA = 0 -> existe uma raízes real
+
+#Se DELTA > 0 -> existem duas raízes reais
+
+
+   #Raízes -> (-b +-  RAIZ DE DELTA) /2.a
+		 
+=============================================================================================================================
+#2)	Solicitar dois valores inteiros, e mostrá-los na ordem crescente e decrescente.
+  
+==============================================================================================================================
+#4) Solicitar um valor inteiro positivo (consistir) e mostrar se esse número é par ou não par 
+  
+==============================================================================================================================
+#5) Solicitar dois valores inteiros, e mostrá-los na ordem crescente e decrescente (prever para quando os valores forem iguais.)
+
+===============================================================================================================================
+#6) Receber 3 valores numéricos, X, Y e Z, e verificar se esses valores podem corresponder aos lados de um triângulo. Em caso 
+#afirmativo, informar ao usuário se o triângulo é equilátero, isósceles ou escaleno.
+
+================================================================================================================================
+#7) Receber 3 valores numéricos em 3 variáveis, A, B e C, e trocar os valores entre as variáveis de forma que, ao final do algoritmo, 
+#a variável A possua o menor valor e a variável C o maior.
+  
+================================================================================================================================
+#8) Receber valores de base e altura de um triângulo e verificar se são valores válidos (positivos maiores que zero). Em caso afirmativo, calcular a área do triângulo.
+
+
+================================================================================================================================
+
+#9) Dado um número inteiro de segundos, mostrar a quantas horas, minutos e segundos ele corresponde.
+
+================================================================================================================================  
+#10) Credito especial em função do saldo médio.
+	#- ler o saldo medio do cliente,
+	#- calcular o valor do credito em função da tabela a seguir:
+			#SALDO MÉDIO		% CREDITO
+			#0 a 200				0%
+			#201 a 400			20%
+			#401 a 600			30%
+			#Acima de 601	40%
+	
+#	- mostrar como saída uma mensagem informando o saldo médio e o valor do crédito
+================================================================================================================================
+#11) Escrever um algoritmo que lê um conjunto de 4 valores i, a, b, c, onde i é um valor inteiro e positivo e a, b, c, são quaisquer valores reais e os escreva. A seguir: Se i=1 escrever os três valores a, b, c em ordem crescente. 
+   # Se i=2 escrever os três valores a, b, c em ordem decrescente. 
+    #Se i=3 escrever os três valores a, b, c de forma que o maior entre a, b, c fique dentre os dois. 
+
+================================================================================================================================  
+#12) Um vendedor necessita de um algoritmo que calcule o preço total devido por um cliente. O algoritmo deve receber o código de um produto e a quantidade comprada e calcular o preço total, usando a tabela abaixo: 
+
+#Código do produto Preço unitário 
+#1 		R$ 32,00 
+#2 		R$ 45,00 
+#3		  R$ 37,00 
+#4 		R$ 33,00 
+ 	
+================================================================================================================================  
+#13) Escrever um algoritmo que lê um valor em Reais (R$) e calcule qual o menor número possível de notas de 100, 50, 10, 5 e 1 em que o valor lido pode ser decomposto. Escrever o valor lido e a relação de notas necessárias. 
+
+================================================================================================================================  
+#14) Escrever um algoritmo que lê um conjunto de 4 valores i, a, b, c, onde i é um valor inteiro e positivo e a, b, c, são quaisquer valores reais e os escreva. A seguir: 
+    #Se i=1 escrever os três valores a, b, c em ordem crescente. 
+    #Se i=2 escrever os três valores a, b, c em ordem decrescente. 
+    #Se i=3 escrever os três valores a, b, c de forma que o maior entre a, b, c fique dentre os dois. 
+
+================================================================================================================================  
+#15) As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra. 
+
+  
